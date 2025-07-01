473c4fd6b35fb187d9300f910c8636271c659c69
ig-i <gubernatorov_i@mail.ru>
2025-07-02 00:08:32 +0300

    july-01

--- first/password_generator_project/generator/views.py
+++ first/password_generator_project/generator/views.py
@@ -5,24 +5,29 @@ import random
 
 def home(request):
     lst = list(range(6, 15))
-    return render(request, "generator/home.html",{'lst': lst})
+    return render(request, "generator/home.html", {'lst': lst})  # подключаем html страницу
 
 
 def password(request):
-    char = [chr(i) for i in range(97, 123)]
-    if request.GET.get('uppercase'):
+    char = [chr(i) for i in range(97, 123)]  # генерируем коды символов в нижнем регистре в указанном интервале
+    # print(char)
+    if request.GET.get('uppercase'):         # генерируем коды символов в верхнем регистре в заданном интервале
         char.extend([chr(i) for i in range(65, 91)])
 
-    if request.GET.get('numbers'):
+    if request.GET.get('numbers'):                     # генерируем числовые коды символов
         char.extend([chr(i) for i in range(48, 58)])
 
-    if request.GET.get('special'):
+    if request.GET.get('special'):                     # генерируем спецсимволы
         char.extend([chr(i) for i in range(33, 48)])
 
-
-    length = int(request.GET.get("length"))
+    length = int(request.GET.get("length"))             # получаем данные из home.html name='length' (кол-во символов)
 
     psw = ''
     for i in range(length):
-        psw += random.choice(char)
+        psw += random.choice(char)                          # возвращает случайную последовательность элементов
     return render(request, 'generator/password.html', {'password': psw})
+
+
+def norms(request):
+    # nrw = list(range(6, 15))
+    return render(request, "generator/norms.html")
--- second/personal_portfolio/personal_portfolio/urls.py
+++ second/personal_portfolio/personal_portfolio/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for password_generator_project project.
+URL configuration for personal_portfolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from generator import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+from skills import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
-    path('password', views.password, name="password"),
-    path('norms', views.norms, name="norms"),
-
+    path('', views.index, name='index')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
